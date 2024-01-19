beb4f3ffc6565b8c0f14cc4e2d8ba8384c69a8b7
joooons <52220710+joooons@users.noreply.github.com>
2024-01-18 21:39:51 -0800

    modify TextInput attributes

--- main.py
+++ main.py
@@ -3,15 +3,25 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
+
 class LoginScreen(GridLayout):
     def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.cols = 2
-        self.add_widget(Label(text='Username'))
-        self.username = TextInput(multiline=False)
+        self.add_widget(Label(text='Username', font_size=72))
+        self.username = TextInput(text='Dude',
+                                  multiline=False,
+                                  padding_x=[100,100],
+                                  padding_y=[200,0],
+                                  font_size=72,
+                                  background_color=[0.8, 1, 0.8, 1])
         self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(Label(text='password', font_size=72))
+        self.password = TextInput(password=True,
+                                  multiline=False,
+                                  padding=[10, 10, 10, 10],
+                                  font_size=72
+                                  )
         self.add_widget(self.password)
 
 class MyApp(App):
