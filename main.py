cde1ac376219184816b5a00de0451fc03d7b3a82
joooons <52220710+joooons@users.noreply.github.com>
2024-02-01 14:33:43 -0800

    use kivy design language to make button print

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from kivy.app import App
 
 from kivy.uix.widget import Widget
-from kivy.uix.label import Label
+# from kivy.uix.label import Label
+from kivy.properties import ObjectProperty
 
 
 class MyGridLayout(Widget):
-    def press(self, instance):
+
+    name = ObjectProperty(None)
+    color = ObjectProperty(None)
+    pizza = ObjectProperty(None)
+    def press(self):
         name = self.name.text
         color = self.color.text
         pizza = self.pizza.text
-        self.add_widget(Label(text=f'{name} {color} {pizza}'))
+
+        print(f'{name} {color} {pizza}')
+        # self.add_widget(Label(text=f'{name} {color} {pizza}'))
         self.name.text = ''
         self.color.text = ''
         self.pizza.text = ''
