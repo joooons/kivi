a610e9b68e34688957e03c4a002ecba54161870c
joooons <52220710+joooons@users.noreply.github.com>
2024-02-01 23:32:57 -0800

    initialize menu page and game page

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
 from kivy.app import App
-
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
 from kivy.lang import Builder
-from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_file('unrelated.kv')
+Builder.load_file('manager.kv')
 
 
-class MyLayout(Widget):
+class MenuScreen(Screen):
+    pass
 
-    name = ObjectProperty(None)
-    color = ObjectProperty(None)
-    pizza = ObjectProperty(None)
-    def press(self):
-        name = self.name.text
-        color = self.color.text
-        pizza = self.pizza.text
-
-        print(f'{name} {color} {pizza}')
-        # self.add_widget(Label(text=f'{name} {color} {pizza}'))
-        self.name.text = ''
-        self.color.text = ''
-        self.pizza.text = ''
-
+class GameScreen(Screen):
+    pass
 
 class Space(App):
-    def build(self):
-        Window.clearcolor = (0.7,0.9,0.7,1)
-        return MyLayout()
 
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(GameScreen(name='game'))
+        return sm
 
 if __name__ == '__main__':
     Space().run()
