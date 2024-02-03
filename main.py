0935850cc602a34a7fdf165120ef416c6ecb18b0
joooons <52220710+joooons@users.noreply.github.com>
2024-02-02 22:00:11 -0800

    rotate sprite

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from kivy.animation import Animation
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-from kivy.graphics import Rectangle, Color
+from kivy.graphics import Rectangle, Color, PushMatrix, Rotate, PopMatrix
 
 Builder.load_file('menu.kv')
 Builder.load_file('game.kv')
@@ -17,8 +18,20 @@ class GameScreen(Screen):
 
 
 class Space(App):
-    square_pos = {'x': 0, 'y': 400}
-    angle = -45
+    square_pos = {'x': 400, 'y': 700}
+
+    def rotate_image(self, instance) -> None:
+        print('it work')
+        print(instance)
+        # animate = Animation(pos=(500, 400), size=(300, 300), duration=1)
+        # animate.start(instance)
+        with instance.canvas.before:
+            PushMatrix()
+            Rotate(angle=-10, origin=instance.center, axis=(0, 0, 1))
+        with instance.canvas.after:
+            PopMatrix()
+
+
     def build(self):
         sm = ScreenManager(transition=NoTransition())
         sm.add_widget(GameScreen(name='game'))
