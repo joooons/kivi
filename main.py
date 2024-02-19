557fcd201f4388fc1cb9af2996217e17093c98e1
joooons <52220710+joooons@users.noreply.github.com>
2024-02-19 10:10:22 -0800

    set window size to 400, 600 for now

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import kivy
+from kivy.config import Config
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 
 kivy.require("2.2.1")
+
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '600')
 
 Builder.load_file('menu.kv')
 Builder.load_file('tick.kv')
